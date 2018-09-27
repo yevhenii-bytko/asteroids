0366e791b33a887df955e700e142eabc8e87078a
Yevhenii Bytko <yevhenii.bytko@gmail.com>
2018-09-27 20:01:24 +0300

    refactor asteroids belt physics and render

--- bundle.js
+++ bundle.js
@@ -1,6 +1,6 @@
 // GLOBAL VARIABLES
 let canvas, ctx;
-let rocketShip;
+let rocketShip, asteroidsBelt;
 
 // CONSTANTS
 const FPS = 30;
@@ -12,12 +12,33 @@ const ROCKET_SHIP_TURN_ANGLE = 360;
 const THRUST_FORCE = 5;
 const FRICTION = 0.5;
 
+const ASTEROIDS_NUMBER = 3;
+const ASTEROIDS_MIN_SPEED = 4;
+const ASTEROIDS_MAX_SPEED = 40;
+const ASTEROIDS_VERTICES_NUMBER = 10;
+const JAGGEDNESS = 0.4
+
 // DATA STRUCTURES
 function RocketShip(shape, rotation, thrust) {
     this.shape = shape;
     this.rotation = rotation;
     this.thrust = thrust;
 }
+
+function Asteroid(polygon, rotation, direction) {
+    this.polygon = polygon;
+    this.rotation = rotation;
+    this.direction = direction;
+    this.offsets = [];
+}
+
+function Polygon(point, radius, verticesNumber) {
+    Shape.call(this, point, radius);
+    this.verticesNumber = verticesNumber;
+}
+
+Polygon.prototype = Object.create(Shape.prototype);
+Polygon.prototype.constructor = Polygon;
 
 function Shape(point, radius) {
     this.point = point;
@@ -35,8 +56,15 @@ function Rotation(angle, increment = 0) {
 }
 
 function Thrust(point) {
-    this.point = point;
+    Direction.call(this, point);
     this.isThrustung = false;
+}
+
+Thrust.prototype = Object.create(Direction.prototype);
+Thrust.prototype.constructor = Thrust;
+
+function Direction(point) {
+    this.point = point;
 }
 
 // ENTRY POINT
@@ -53,12 +81,13 @@ window.onload = function () {
 
 function startNewGame() {
     initRocketShipBaseInstance();
+    initAsteroidsBeltInstance(ASTEROIDS_NUMBER);
 }
 
 function update() {
-    // draw space background;
     drawSpaceBg();
     updateRocketShipPhysicsAndRender();
+    updateAsteroidsBeltPhysicsAndRender();
 }
 
 // KEY-LISTENERS
@@ -112,7 +141,7 @@ function handleRocketShipPosition() {
     rotateRocketShip();
     moveRocketShip();
     thrustRocketShip();
-    handleEdgeOfScreen(rocketShip.shape.point);
+    handleEdgeOfScreen(rocketShip.shape.point, rocketShip.shape.radius);
 }
 
 function rotateRocketShip() {
@@ -160,6 +189,68 @@ function disableRocketShipThrust() {
 
 function isRocketShipThrust() {
     return rocketShip.thrust.isThrustung;
+}
+
+function initAsteroidsBeltInstance(asteroidNumber) {
+    asteroidsBelt = [];
+    for (let i = 0; i < asteroidNumber; i++) {
+        const point = getRandomPointExcludingRadiusZone(rocketShip.shape.point);
+        asteroidsBelt.push(createRandomAsteroid(point, BASE_SIZE * 2));
+    }
+}
+
+function createRandomAsteroid(point, radius) {
+    const asteroid = new Asteroid(
+        new Polygon(point, radius, getRandomVerticesNumber()),
+        new Rotation(getRandomInt(0, ROCKET_SHIP_TURN_ANGLE)),
+        new Direction(
+            new Point(
+                getRandomNumberSign(getRandomInt(ASTEROIDS_MIN_SPEED, ASTEROIDS_MAX_SPEED)),
+                getRandomNumberSign(getRandomInt(ASTEROIDS_MIN_SPEED, ASTEROIDS_MAX_SPEED))
+            )
+        )
+    );
+    for (let i = 0; i < asteroid.polygon.verticesNumber; i++) {
+        asteroid.offsets.push(getRandomJaggednessValue());
+    }
+    return asteroid;
+}
+
+function getRandomVerticesNumber() {
+    return  Math.floor(Math.random() * (ASTEROIDS_VERTICES_NUMBER + 1) + ASTEROIDS_VERTICES_NUMBER / 2);
+}
+
+function getRandomJaggednessValue() {
+    return Math.random() * JAGGEDNESS * 2 + 1 - JAGGEDNESS;
+}
+
+function updateAsteroidsBeltPhysicsAndRender() {
+    handleAsteroidsPositions();
+    drawAsteroidsBelt(asteroidsBelt);
+}
+
+function handleAsteroidsPositions() {
+    moveAsteroids();
+    accelerateAsteroids();
+}
+
+function moveAsteroids() {
+    for (let i = 0; i < asteroidsBelt.length; i++) {
+        const point = asteroidsBelt[i].polygon.point;
+        const radius = asteroidsBelt[i].polygon.radius;
+        const direction = asteroidsBelt[i].direction;
+        point.x += direction.point.x / FPS;
+        point.y += direction.point.y / FPS;
+        handleEdgeOfScreen(point, radius);
+    }
+}
+
+function accelerateAsteroids(accelerationNumber = 0) {
+    for (let i = 0; i < asteroidsBelt.length; i++) {
+        const direction = asteroidsBelt[i].direction;
+        direction.point.x = direction.point.x + accelerationNumber;
+        direction.point.y = direction.point.y + accelerationNumber;
+    }
 }
 
 // RENDER FUNCTIONS
@@ -220,21 +311,70 @@ function drawThrustTriangles(rocketShip) {
     ctx.stroke();
 }
 
+function drawAsteroidsBelt(asteroidsBelt) {
+    for (let i = 0; i < asteroidsBelt.length; i++) {
+        drawAsteroidPolygon(asteroidsBelt[i]);
+    }
+}
+
+function drawAsteroidPolygon(asteroid) {
+    const point = asteroid.polygon.point;
+    const radius = asteroid.polygon.radius;
+    const angle = asteroid.rotation.angle;
+    const verticesNumber = asteroid.polygon.verticesNumber;
+    const offsets = asteroid.offsets;
+    ctx.strokeStyle = 'slategrey';
+    ctx.lineWidth = BASE_SIZE / 20;
+    ctx.beginPath();
+    ctx.moveTo(
+        point.x + radius * offsets[0] * Math.cos(angle),
+        point.y + radius * offsets[0] * Math.sin(angle)
+    );
+    for (let i = 0; i < verticesNumber; i++) {
+        ctx.lineTo(
+            point.x + radius * offsets[i] * Math.cos(angle + i * 2 * Math.PI / verticesNumber),
+            point.y + radius * offsets[i] * Math.sin(angle + i * 2 * Math.PI / verticesNumber)
+        );
+    }
+    ctx.closePath();
+    ctx.stroke();
+}
+
 // UTILS
 function convertToRadians(angle) {
     return angle * Math.PI / 180;
 }
 
-function handleEdgeOfScreen(point) {
-    if (point.x < 0) {
-        point.x = canvas.width;
-    } else if (point.x > canvas.width) {
-        point.x = 0;
+function handleEdgeOfScreen(point, radius) {
+    if (point.x < 0 - radius) {
+        point.x = canvas.width + radius;
+    } else if (point.x > canvas.width + radius) {
+        point.x = 0 - radius;
     }
 
-    if (point.y < 0) {
-        point.y = canvas.height;
-    } else if (point.y > canvas.height) {
-        point.y = 0;
+    if (point.y < 0 - radius) {
+        point.y = canvas.height + radius;
+    } else if (point.y > canvas.height + radius) {
+        point.y = 0 - radius;
     }
+}
+
+function getRandomInt(min = 0, max = 100) {
+    return Math.floor(Math.random() * (max - min) + min);
+}
+
+function getRandomNumberSign(number) {
+    return Math.random() < 0.5 ? number : -number;
+}
+
+function getDistanceBetweenPoints(point1, point2) {
+    return Math.sqrt(Math.pow(point1.x - point2.x, 2) + Math.pow(point1.y - point2.y, 2));
+}
+
+function getRandomPointExcludingRadiusZone(point) {
+    let finalPoint;
+    do {
+        finalPoint = new Point(getRandomInt(0, canvas.width + 1), getRandomInt(0, canvas.height + 1))
+    } while (getDistanceBetweenPoints(point, finalPoint) < BASE_SIZE * 6);
+    return finalPoint
 }
